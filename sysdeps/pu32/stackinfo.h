f4e579050b2699e51583d0ead34d8c061d9a766b
William Tambe <tambewilliam@gmail.com>
2019-08-30 00:39:09 -0500

    Target PU32 support

---
+++ sysdeps/pu32/stackinfo.h
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-2.0-only
+// (c) William Fonkou Tambe
+
+#ifndef _STACKINFO_H
+#define _STACKINFO_H 1
+
+#include <elf.h>
+
+#define _STACK_GROWS_DOWN 1
+
+#define DEFAULT_STACK_PERMS (PF_R | PF_W)
+
+#endif	/* _STACKINFO_H */
