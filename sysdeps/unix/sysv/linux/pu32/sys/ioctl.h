6991dc956e661bf088454acde6b29d600f12f426
William Tambe <tambewilliam@gmail.com>
2022-09-15 18:30:00 -0500

    ioctl.h bugfix

--- sysdeps/unix/sysv/linux/pu32/sys/ioctl.h
+++ sysdeps/unix/sysv/linux/pu32/sys/ioctl.h
@@ -26,8 +26,8 @@ __BEGIN_DECLS
    Return value depends on REQUEST.  Usually -1 indicates error.  */
 extern int __ioctl (int __fd, unsigned long __request, void *__arg) __THROW;
 #define __IOCTL_VARIANT(_1,_2,_3,_X,...) _X
-#define __IOCTL2(fd,req) __ioctl(fd,req,(void*)0)
-#define __IOCTL3(fd,req,arg) __ioctl(fd,req,(void*)arg)
+#define __IOCTL2(fd,req) __ioctl((fd),(req),(void*)0)
+#define __IOCTL3(fd,req,arg) __ioctl((fd),(req),(void*)(arg))
 #define ioctl(...) __IOCTL_VARIANT(__VA_ARGS__, __IOCTL3, __IOCTL2)(__VA_ARGS__)
 
 __END_DECLS
