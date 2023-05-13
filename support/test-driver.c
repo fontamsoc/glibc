ba3e0c331c0c48dab78fbee60c9a383d41a1f11b
William Tambe <tambewilliam@gmail.com>
2023-05-12 20:54:46 -0500

    Revert "elf: Testing infrastructure for ld.so DSO sorting (BZ #17645)"
    This reverts commit e6fd79f3795d46dfb583e124be49fc063bc3d58b.

--- support/test-driver.c
+++ support/test-driver.c
@@ -116,9 +116,7 @@ main (int argc, char **argv)
 #if defined (TEST_FUNCTION) && defined (TEST_FUNCTON_ARGV)
 # error TEST_FUNCTION and TEST_FUNCTION_ARGV cannot be defined at the same time
 #endif
-#ifdef RUN_COMMAND_MODE
-  test_config.run_command_mode = 1;
-#elif defined (TEST_FUNCTION)
+#if defined (TEST_FUNCTION)
   test_config.test_function = TEST_FUNCTION;
 #elif defined (TEST_FUNCTION_ARGV)
   test_config.test_function_argv = TEST_FUNCTION_ARGV;
